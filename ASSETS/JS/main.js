78cbb99d6b52e5fb9d07707c72bb132d15ba93c9
gbelda <gbelda56@gmail.com>
2021-10-22 13:56:12 +0200

    Added team array and members

---
+++ ASSETS/JS/main.js
@@ -0,0 +1,33 @@
+let team = [
+    {
+        name: 'Richard Hendricks',
+        role: 'CEO',
+        picture: '../../IMG/richard.png',
+    },
+
+    {
+        name: 'Jared Dunn',
+        role: 'Chief Operating Officer',
+        picture: '../../IMG/jared.png'
+
+    },
+    {
+        name: 'Dinesh Chugtai',
+        role: 'Senior Programmer',
+        picture: '../../IMG/dinesh.png'
+
+    },
+    {
+        name: 'Bertram Gilfoyle',
+        role: 'Senior Systems Architect',
+        picture: '../../IMG/gilfoyle.png'
+
+    },
+    {
+        name: 'Erlich Bachman',
+        role: 'Board Member',
+        picture: '../../IMG/erlich.png'
+
+    }
+]
+console.log(team);
